05318a47715c28904f0a986fe405930e726fa1e1
manat <mml21122012@gmail.com>
2022-09-10 20:42:45 +0300

    refactor docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,30 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import * as path from 'path';
 import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication, ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from 'app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 const PORT = Number.isNaN(Number(process.env.PORT)) ? 9000 : Number(process.env.PORT);
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(), {
-    logger: ['error', 'log', 'warn'],
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors();
   app.use(cookieParser(process.env.COOKIE_SECRET));
-
   app.setGlobalPrefix('api');
-
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
   app.useStaticAssets(path.join(__dirname, '../..', 'assets'), {
     index: false,
     prefix: '/public',
   });
 
-  await app.listen(PORT, '0.0.0.0');
+  await app.listen(PORT, () => {
+    console.log(`Server started by ${PORT} port`);
+  });
 }
 
-bootstrap().then(() => {
-  console.log(`Server started by ${PORT} port`);
-});
+bootstrap().then();
