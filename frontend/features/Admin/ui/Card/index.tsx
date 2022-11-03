65bd142e90a7f3eaf94ec80c383292b3be76c5dc
manat <mml21122012@gmail.com>
2022-11-03 12:58:13 +0300

    down

--- frontend/features/Admin/ui/Card/index.tsx
+++ frontend/features/Admin/ui/Card/index.tsx
@@ -8,7 +8,11 @@ import { CardProps } from '@features/Admin/ui/Card/types'
 import { Pagination } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Photo } from '@models/Products'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 import styles from './index.module.scss'
+
+const MySwal = withReactContent(Swal)
 
 const Card: FC<CardProps> = ({
   id,
@@ -30,7 +34,20 @@ const Card: FC<CardProps> = ({
   }
 
   const handleRemove = async (id: string) => {
-    remove?.(id)
+    const { isConfirmed } = await MySwal.fire({
+      title: 'Вы уверены?',
+      text: 'Вы действительно хотите удалить?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#e23535',
+      cancelButtonColor: '#24822C',
+      confirmButtonText: 'Да, удалить!',
+      cancelButtonText: 'Отмена',
+    })
+
+    if (isConfirmed) {
+      remove?.(id)
+    }
   }
 
   useEffect(() => {
